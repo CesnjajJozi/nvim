b5e2bd06dbe3fe0b8bc813f42c6262a0f3b11f93
jozi <cesnjaj.jozi@gmail.com>
2024-07-03 08:26:33 +0200

    Added Git ignore configuration for node modeules in init.lua

--- init.lua
+++ init.lua
@@ -352,12 +352,12 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+          },
+        },
+
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
